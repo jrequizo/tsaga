5ea52fbe5d4189b8b7ed0f4648a49e2be15604e1
Jay Requizo <jay.requizo@outlook.com>
2025-08-11 00:48:56 +1000

    WIP: exposing 'router' to the 'emit' property in the 'createSaga' function

--- src/createSagaCaller.ts
+++
@@ -1,9 +0,0 @@
-
-/**
- * Creates a minimally exposed caller for the Saga that can be safely passed and used within Sagas
- * 
- * TODO: we need to pass in the SagaRouter and SagaBuilder
- */
-export default function createSagaCaller() {
-
-}
